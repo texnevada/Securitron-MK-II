ef4f4ae40a9ece975f7ed86e20b9b98a457bd104
texnevada <15308761+texnevada@users.noreply.github.com>
2019-06-25 11:17:32 +0200

    added purge command

---
+++ admin/purgecommand.py
@@ -0,0 +1,27 @@
+#import discord py library
+import discord
+#Imports commands
+from discord.ext import commands
+#import permissions
+from discord.ext.commands import has_permissions
+import sqlite3
+
+class purgecommand(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    #Reads the users input if it has prefix or not
+    @commands.command(name="clear", aliases=["purge"])
+    #Will only execute command if user has the role
+    @has_permissions(manage_channels=True)
+    #sees user wants to use the command clear.
+    async def clear(self, ctx, amount: int):
+        try:
+            await ctx.channel.purge(limit=amount+1)
+            await ctx.channel.send("Messages cleared!")
+            print(f"Purged messages in \"{ctx.guild.name}\" ")
+        except Exception as e:
+            await ctx.send("I require the permission \"manage channels\" to delete messages for you.")
+
+def setup(client):
+    client.add_cog(purgecommand(client))
